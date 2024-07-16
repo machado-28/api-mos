3c492c8906151841a3d0a4ab1666e0e4a2da782a
António Machado <dev.antoniomachado@gmail.com>
2024-07-16 12:06:48 +0100

    test actions

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ class App {
       },)
     );
     this.server.use("/ficheiros",
-      express.static(path.resolve(__dirname, "..", "temp", "upload")),)
+      express.static(path.resolve(__dirname, "..", "temp", "uploads")),)
     this.server.use("/public",
       express.static(path.resolve(__dirname, "..", "public")),)
   }
